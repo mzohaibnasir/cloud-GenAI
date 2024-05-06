428cae07fc1981e4fcfec4d7bd161b0b80a79aaa
M-Zohaib Nasir <m.zohaibnasir6@gmail.com>
2024-05-06 23:24:29 +0500

    list endpoint

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -4,6 +4,13 @@ import uvicorn
 
 fastapp = FastAPI()
 
+# creating list endpoint
+BANDS = [
+    {"id": 1, "name": "A", "genre": "X"},
+    {"id": 2, "name": "B", "genre": "Y"},
+    {"id": 3, "name": "C", "genre": "Z"},
+]
+
 
 @fastapp.get("/")
 async def index() -> dict[str, int]:
@@ -15,6 +22,13 @@ async def about() -> str:
     return "An exceptional Company"
 
 
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:fastapp", host="127.0.0.1", port=5000, log_level="info", reload=True
+    )
+
+
+############################################################################
 """
 The command uvicorn main:app refers to:
     main: the file main.py (the Python "module").
@@ -23,11 +37,5 @@ The command uvicorn main:app refers to:
 It is equivalent to:
     `from main import app`
 """
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:fastapp", host="127.0.0.1", port=5000, log_level="info", reload=True
-    )
 
 """ or uvicorn main:app --reload"""

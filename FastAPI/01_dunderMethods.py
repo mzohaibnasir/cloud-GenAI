26569554154a12a001d58866245ad1f2ad78a588
M-Zohaib Nasir <m.zohaibnasir6@gmail.com>
2024-05-06 18:20:19 +0500

    up

--- FastAPI/01_dunderMethods.py
+++ FastAPI/01_dunderMethods.py
@@ -1,12 +1,22 @@
 class Person:
-    def __init__(self, name: str = None, age: int = None):
-        self._name = name
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
         self.age = age
 
-    def __del__(self):
-        print("Obj is being distructed")
+    # def __del__(self):
+    #     print("object is being destructed")
+
+    def __add__(self, other):
+        self.name = f"{self.name}+{other.name}"
+        self.age = self.age + other.age
+        return self.name, self.age
 
 
+z = Person(name="x", age=1)
 p = Person(name="Mike", age=10)
-print(p._name, p.age)
+q = Person(name="Leonard", age=100)
+print(p.name, p.age)
+print(q.name, q.age)
+z = p + q
+print(z.name, z.age)
 del p

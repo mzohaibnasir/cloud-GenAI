78b9b657442e86a4a5e72cbc6b747527211ec883
M-Zohaib Nasir <m.zohaibnasir6@gmail.com>
2024-05-06 21:40:59 +0500

    generator

---
+++ FastAPI/08_generators.py
@@ -0,0 +1,14 @@
+"""
+# Generators:
+A generator is a special type of function that returns an iterator, which produces a sequence of values on demand. 
+Instead of computing all values at once, a generator computes each value only when its next() method is called. 
+This allows for efficient handling of large datasets, as only the current value is stored in memory.
+
+
+# Lazy Execution:
+Lazy execution is a technique where the evaluation of an expression is delayed until its value is actually needed. 
+In Python, lazy execution is implemented using generators and iterators. When you create a generator or iterator, 
+the code inside the generator or iterator is not executed until you start iterating over it. 
+This allows for efficient handling of large datasets, as the code is only executed when needed.
+    
+"""

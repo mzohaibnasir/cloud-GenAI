23b7a85837507036c7a0a472489c05ee54792d29
M-Zohaib Nasir <m.zohaibnasir6@gmail.com>
2024-05-11 15:41:36 +0500

    mongoDb

---
+++ FARM-Stack/README.md
@@ -0,0 +1,15 @@
+# F-A-R-M STACK
+
+It is based on document model i.e. collection of docs
+
+# MongoDB is a NoSQL database
+
+that uses a document-oriented data model, where data is stored in JSON-like documents called BSON (Binary Serialized Object Notation). This allows for flexible and dynamic schema design, and makes it well-suited for handling large amounts of semi-structured or unstructured data.
+In MongoDB, data is organized into collections, which are similar to tables in relational databases. Each collection contains a set of documents, which are similar to rows in a relational database. However, unlike relational databases, MongoDB documents can have different fields and structures, and can be modified dynamically without requiring a fixed schema.
+Some key features of MongoDB's document model include:
+
+1. Flexible schema: MongoDB's schema is dynamic and can be modified at runtime, allowing for easy adaptation to changing data structures and requirements.
+2. Hierarchical data: MongoDB's document model allows for hierarchical data structures, where documents can contain embedded documents and arrays.
+3. Scalability: MongoDB is designed for horizontal scaling, making it suitable for large and distributed datasets.
+4. High performance: MongoDB's document model and indexing capabilities allow for fast query performance and data retrieval.
+   Overall, MongoDB's document model and NoSQL design make it a popular choice for modern web and mobile applications, big data analytics, and real-time data processing.
--- FastAPI/test.py
+++ FastAPI/test.py
@@ -1,4 +1,4 @@
-from typing import Annotated,get_type_hints, get_origin,get_args
+from typing import Annotated, get_type_hints, get_origin, get_args
 from fastapi import Query
 from functools import wraps
 
@@ -16,41 +16,41 @@ from functools import wraps
 #         print(f"Value: {hint_args}")
 #         miny,maxy= hint_args[0]
 #         print(f"min: {miny}, max: {maxy}")
-        
+
 #         if not (miny<=x<=maxy):
 #             raise ValueError(f"{x} falls outside of the boundary {miny}->{maxy}")
-            
+
 #     return x*2
 
 
 def check_value_range(func):
     @wraps(func)
     def wrapped(x):
-        type_hints = get_type_hints(double,include_extras=True)
+        type_hints = get_type_hints(double, include_extras=True)
         print(type_hints)
-        hint = type_hints['x']
-        print("x: ",hint)
+        hint = type_hints["x"]
+        print("x: ", hint)
         if get_origin(hint) is Annotated:
-            hint_type,*hint_args = get_args(hint)
+            hint_type, *hint_args = get_args(hint)
             print(f"HINT: {hint_type}")
             print(f"Value: {hint_args}")
-            miny,maxy= hint_args[0]
+            miny, maxy = hint_args[0]
             print(f"min: {miny}, max: {maxy}")
-            
-            if not (miny<=x<=maxy):
+
+            if not (miny <= x <= maxy):
                 raise ValueError(f"{x} falls outside of the boundary {miny}->{maxy}")
-        
+
         return func(x)
+
     return wrapped
 
 
-
 @check_value_range
-def double(x:Annotated[int, (0,10)])->int:
-    return x*2
+def double(x: Annotated[int, (0, 10)]) -> int:
+    return x * 2
 
 
-result=double(11)
+result = double(1)
 print(result)
 
 
